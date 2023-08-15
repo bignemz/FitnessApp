c77feba1258c0217f0390bbe4e46972fc1d329e4
bignemz <chinaemeemerole@yahoo.com>
2023-08-15 10:48:45 +0100

    add

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Introduction from '../../components/Introduction/Introduction'
 import About from '../../components/AboutUs/About'
+import './Homepage.scss'
 
 function Homepage() {
   return (
-    <div>
+    <div className='Home'>
         <Introduction/>
         <About/>
     </div>
